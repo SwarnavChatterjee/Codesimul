b44f50a4aa89556f82c9088ebeae153e7a2c037e
SwarnavChatterjee <swarnavchatterjee@icloud.com>
2026-01-18 23:21:50 +0530

    fix express 5 wildcard route

--- backend/index.js
+++ backend/index.js
@@ -141,7 +141,7 @@ const buildPath = path.join(__dirname, "..", "build");
 app.use(express.static(buildPath));
 
 // React routing fallback
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(buildPath, "index.html"));
 });
 
